e58479a0b55307808dcd42de4f0ebc02a3b602bc
Roman Sherstnev <sher-rom2013@yandex.ru>
2020-09-11 05:45:52 +0700

    Refactored masscan parse script

--- Scripts/masscanparse.py
+++ Scripts/masscanparse.py
@@ -7,21 +7,26 @@ if len(sys.argv) != 2:
 else:
     tcp = list()
     udp = list()
-    with open(sys.argv[1]) as file:
-        for line in file.readlines():
+    with open(sys.argv[1]) as report:
+        for line in report.readlines():
             data = line.split()
             if data[0] == 'open':
-                if data[1] == 'tcp':
-                    tcp.append(int(data[2]))
-                else:
-                    udp.append(int(data[2]))
-    tcp.sort()
-    udp.sort()
-    print('-p T:', end='')
-    for port in tcp:
-        print('{},'.format(port), end='')
-    udp_string = str()
-    print('U:', end='')
-    for port in udp:
-        udp_string = udp_string + '{},'.format(port)
-    print(udp_string.rstrip(','))
+                tcp.append(int(data[2])) if data[1] == 'tcp' else udp.append(int(data[2]))                
+    result = ''
+    if tcp:
+        result += '-sS '
+        tcp.sort()
+    if udp:
+        result += '-sU '
+        udp.sort()
+    if tcp or udp:
+        result += '-p '
+    if tcp:
+        result += 'T:'
+        for port in tcp:
+            result += '{},'.format(port)
+    if udp:
+        result += 'U:'
+        for port in udp:
+            result += '{},'.format(port)
+    print(result.rstrip(','))
